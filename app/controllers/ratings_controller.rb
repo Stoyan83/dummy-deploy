9687e7ea1121e12cd1528749aba68bd668edddb3
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-09-16 17:46:28 +0300

    Rating and bootstrap

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,22 @@
+class RatingsController < ApplicationController
+  def new
+    @rating = Rating.new
+    @average_rating = Rating.average(:value)
+  end
+
+  def create
+    @rating = Rating.new(rating_params)
+    if @rating.save
+      flash[:success] = 'Rating was successfully created.'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:value)
+  end
+end
