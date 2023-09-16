9dff6c51affcf507495ad90790ab24b4db60434f
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-09-17 00:40:57 +0300

    num of records

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,14 @@ class Rating < ApplicationRecord
   validates :value, presence: true, inclusion: { in: 1..5 }
 
   scope :average_rating, -> { average(:value)&.round }
+
+  after_commit :prune_excess_records
+
+  MAX_RECORDS = 50
+
+  private
+
+  def prune_excess_records
+    self.class.first.destroy if self.class.count > MAX_RECORDS
+  end
 end
